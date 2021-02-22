f736b392ce1d3d587bbc3af588be064da27cdb59
Utsabpokharel <pokharelutsab567@gmail.com>
2021-02-22 15:05:20 +0545

    templating

---
+++ resources/views/Admin/layouts/master.blade.php
@@ -0,0 +1,12 @@
+@include('Admin.layouts.head')
+@include('Admin.layouts.header')
+@include('Admin.layouts.index')
+@include('Admin.layouts.sidebar')
+@include('Admin.layouts.footer')
+
+
+@yield('head')
+@yield('header')
+@yield('index')
+@yield('sidebar')
+@yield('footer')
